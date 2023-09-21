c93135af10bc18d8700457a03d24ac0031f4934f
+ <+>
2023-09-21 17:56:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, ${Grace Hopper}, !';
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, ${GRACE HOPPER}, !"
+const shortGreeting = "Welcome, ${G}, !"
